40cd30df89056d26971b8b462a68ef1b35b81d61
JengTallis <alog1024@alog1024s-MacBook-Pro.local>
2016-08-09 10:24:09 +0800

    addButton

--- SimpleUI/app/src/main/java/com/example/jengtallis/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/jengtallis/simpleui/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.jengtallis.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView;
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText("Hello textView!");
+    }
+
+    public void click(View view){
+        textView.setText("view clicked!");
     }
 }
