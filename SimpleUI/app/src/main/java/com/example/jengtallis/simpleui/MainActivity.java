ab00386261be212d7cd1243a80ec13390d855425
JengTallis <alog1024@alog1024s-MacBook-Pro.local>
2016-08-10 11:14:55 +0800

    customize order list view item

--- SimpleUI/app/src/main/java/com/example/jengtallis/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/jengtallis/simpleui/MainActivity.java
@@ -4,16 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     String drink = "Black Tea";
 
-    List<String> data = new ArrayList<String>();
+    List<Order> data = new ArrayList<Order>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +66,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupListView(){
 //        String[] data = new String[]{"0","1","2","3","4","5","6","7","8","9"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        List<Map<String, String>>mapList = new ArrayList<>();
+        for(Order order: data){
+            Map<String, String> item = new HashMap<>();
+
+            item.put("note", order.note);
+            item.put("storeInfo", order.storeInfo);
+            item.put("drink", order.drink);
+
+            mapList.add(item);
+        }
+
+        String[] from = {"note", "storeInfo", "drink"};
+        int[] to = {R.id.noteTextView, R.id.storeInfoTextView, R.id.drinkTextView};
+
+        SimpleAdapter adapter = new SimpleAdapter(this, mapList, R.layout.listview_order_item, from, to);
+
         listView.setAdapter(adapter);
     }
 
@@ -80,7 +97,22 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(str);
         editText.setText("");
 
-        data.add(str);
+        Order order = new Order();
+
+        order.note = name;
+        order.drink = drink;
+        order.storeInfo = (String)spinner.getSelectedItem();
+
+        data.add(order);
         setupListView();
+    }
+
+    /**
+     * Created by alog1024 on 8/10/16.
+     */
+    public static class Order {
+        String note;
+        String storeInfo;
+        String drink;
     }
 }
---
+++ SimpleUI/app/src/main/java/com/example/jengtallis/simpleui/Order.java
@@ -0,0 +1,10 @@
+package com.example.jengtallis.simpleui;
+
+/**
+ * Created by alog1024 on 8/10/16.
+ */
+public class Order {
+    String note;
+    String storeInfo;
+    String drink;
+}
